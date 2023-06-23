d6991513e51ce568e18811fd33089e6223e80bf8
Arthur Makarevich <arthurDiving@gmail.com>
2023-06-23 20:46:01 +0500

    Add files via upload

---
+++ MyersBriggsApp/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace MyersBriggsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(292, 192);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Учетные данные";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(178, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 38);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Начать тест";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(20, 143);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(139, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(20, 95);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(139, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(20, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(139, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Отчество";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Имя";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Фамилия";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 210);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(772, 267);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Тест";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label11.Location = new System.Drawing.Point(18, 69);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 17);
+            this.label11.TabIndex = 10;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.ForeColor = System.Drawing.Color.Teal;
+            this.label10.Location = new System.Drawing.Point(58, 130);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 16);
+            this.label10.TabIndex = 9;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.ForeColor = System.Drawing.Color.Teal;
+            this.label9.Location = new System.Drawing.Point(58, 100);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 16);
+            this.label9.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(463, 202);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 46);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label8.Location = new System.Drawing.Point(18, 42);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 18);
+            this.label8.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.Color.Blue;
+            this.label7.Location = new System.Drawing.Point(295, 16);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 20);
+            this.label7.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(179, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 46);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(100, 96);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Пусто";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(13, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(377, 34);
+            this.label4.TabIndex = 3;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Location = new System.Drawing.Point(311, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(473, 192);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Кто сдает";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.Green;
+            this.label6.Location = new System.Drawing.Point(14, 95);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 18);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Результат: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 489);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Тест по методу Myers-Briggs";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ MyersBriggsApp/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MyersBriggsApp
+{
+    public partial class Form1 : Form
+    {
+        string name = "";
+        string lastname = "";
+        string otch = "";
+
+        int A1, A2;
+        int i = 0;
+        StreamReader read;
+        int ch;
+        string s, Res;
+        string s_o = "";
+        string s_b = "";
+        string s_v = "";
+        byte block;
+        byte otv1 = 0;
+        byte otv2 = 0;
+        byte vo = 0;
+        byte ot = 0;
+        const char met = '#';
+
+        string[] Results = {"ISTJ - ответственный, организатор",
+                                "ISFJ - лояльный, исполнитель",
+                                "INFJ - вдохновляющий созерцатель",
+                                "INTJ - независимый, мыслитель",
+                                "ISTP - прагматичный, мастер на все руки",
+                                "ISFP - некичливый, хороший член команды",
+                                "INFP - благородный, идеалист",
+                                "INTP - концептуальный, мечтатель",
+                                "ESTP - спонтанный, реалист",
+                                "ESFP - великодушный, весельчак",
+                                "ENFP - оптимист, для него люди важнее всего",
+                                "ENTP - исследователь, изобретательная личность",
+                                "ESTJ - администратор, требовательный человек",
+                                "ESFJ - гармоничная личность, друг для всех",
+                                "ENFJ - переговорщик, умеющий убеждать",
+                                "ENTJ - Командующий, лидер"};
+
+        public Form1()
+        {
+            InitializeComponent();
+            groupBox2.Enabled = false;
+            groupBox3.Enabled = false;
+        }
+
+
+        //Кнопка "Начать тест
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (DataVerification())
+            {
+                groupBox1.Enabled = false;
+                groupBox2.Enabled = true;
+                groupBox3.Enabled = true;
+                label4.Text = String.Format(" {0} {1} {2}", name, lastname, otch);
+                Res = "";
+                A1 = 0;
+                A2 = 0;
+                block = 0;
+                read = new StreamReader("data.txt");
+                RunningTest();
+            }
+        }
+
+        //Проверка входных данных
+        private bool DataVerification()
+        {
+           
+            //Имя
+            name = textBox1.Text;
+            //Фамилия
+            lastname = textBox2.Text;
+            //Отчество
+            otch = textBox3.Text;
+
+            if (String.IsNullOrEmpty(name))
+            {
+                MessageBox.Show("Некорректно указано поле Имя!!!");
+                return false;
+            }
+
+            if (String.IsNullOrEmpty(lastname))
+            {
+                MessageBox.Show("Некорректно указано поле Фамилия!!!");
+                return false;
+            }
+
+            if (String.IsNullOrEmpty(otch))
+            {
+                MessageBox.Show("Некорректно указано поле Отчество!!!");
+                return false;
+            }
+
+            return true;
+        }
+        private void Show(string tit, string vorp, string var1, string var2, byte nom)
+        {
+            label7.Text = String.Format("Часть: {0}", tit);
+            label8.Text = String.Format("Вопрос: {0}: {1}", nom, vorp);
+            label9.Text = String.Format("1 - {0}", var1);
+            label10.Text = String.Format("2 - {0}", var2);
+        }
+
+        //Кнопка номер 1
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ch = 1;
+            RunningTest();
+        }
+        //Кнопка номер 2
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ch = 2;
+            RunningTest();
+        }
+
+        private void VAL(string str, ref byte num, ref int err)
+        {
+            string result = "";
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                //Проверяем на число
+                if (char.IsDigit(str[i]))
+                {
+                    result += str[i];
+                }
+                else
+                {
+                    err = i;
+                    break;
+                }
+            }
+            num = Convert.ToByte(result);
+        }
+
+        //Главный тест программы
+        private void RunningTest()
+        {
+
+            do {
+    
+                s = read.ReadLine();
+
+                if (s == null)
+                {
+                    read.Close();
+                    groupBox2.Enabled = false;
+                    for (int j = 0; j < 16; j++)
+                    {
+                        if (Results[j].IndexOf(Res) > -1)
+                        {
+                            s_o = Results[j];
+                        }
+                    }
+
+                    label5.Text = s_o;
+
+
+                    StreamWriter swWrite = new StreamWriter(String.Format("{0}_{1}.txt",name,lastname));
+                    swWrite.WriteLine(s_o);
+                    swWrite.Close();
+                    break;
+                }
+
+                if (s[0] == met)
+                    {
+                        if ((A1 > 0) && (A2 > 0))
+                        {
+                            switch (block)
+                            {
+                                case 1:
+                                    if (A1 > A2)
+                                        Res += 'E';
+                                    else
+                                        Res += 'I';
+                                    break;
+                                case 2:
+                                    if (A1 > A2)
+                                        Res += 'S';
+                                    else
+                                        Res += 'N';
+                                    break;
+                                case 3:
+                                    if (A1 > A2)
+                                        Res += 'T';
+                                    else
+                                        Res += 'F';
+                                    break;
+                                case 4:
+                                    if (A1 > A2)
+                                        Res += 'J';
+                                    else
+                                        Res += 'P';
+                                    break;
+                            }
+                            A1 = 0;
+                            A2 = 0;
+                        }
+                        block++;
+                        vo = 0;
+                        s_b = s.Substring(1);
+                        s = read.ReadLine();
+                        s_b = String.Format("{0} - {1}", s_b, s.Substring(1));
+                    }
+                    else if (s.IndexOf(met) == -1)
+                    {
+                        s_v = s;
+                        ot = 0;
+                    }
+                    else if (s.IndexOf(met) > 1)
+                    {
+                        ot++;
+                        vo++;
+
+                        VAL(s.Substring(s.IndexOf(met) + 1), ref otv1, ref i);
+                        s_o = s.Substring(0, s.IndexOf(met));
+                        s = read.ReadLine();
+                        VAL(s.Substring(s.IndexOf(met) + 1), ref otv2, ref i);
+                        Show(s_b, s_v, s_o, s.Substring(0, s.IndexOf(met)), vo);
+
+                        label11.Text = "Нажмите 1 или 2 для ввода ответа. ";
+
+                        switch (ch)
+                        {
+                            case 1:
+                                A1 += otv1;
+                                break;
+                            case 2:
+                                A2 += otv2;
+                                break;
+                        }
+                    }
+
+            } while ((s[0] == met) || (s.IndexOf(met) == -1));
+
+        
+        }
+    }
+}
